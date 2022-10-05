a67564398ce3fac57828f8947bd1b9fd9636d1a1
jjgouveia <gouvik.dev@gmail.com>
2022-10-04 21:50:51 -0300

    requisito 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { readTalkers, findTalkerById } = require('./utils/fsUtils');
 const generateToken = require('./utils/token');
+const emailValidation = require('./middlewares/emailValidation');
+const passwordValidation = require('./middlewares/passwordValidation');
 
 const app = express();
 app.use(bodyParser.json());
@@ -33,7 +35,7 @@ app.get('/talker/:id', async (req, res) => {
   res.status(HTTP_OK_STATUS).json(talker);
 });
 
-app.post('/login', (_req, res) => {
+app.post('/login', emailValidation, passwordValidation, (_req, res) => {
   const token = generateToken();
   res.status(HTTP_OK_STATUS).json({ token });
 });
